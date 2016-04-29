0ec667f2a33ff1722863baf29c6f2f18cc92bc23
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-04-29 22:57:03 +0530

    Ex3.py

---
+++ Week5/List_Slicing/Ex3.py
@@ -0,0 +1,8 @@
+"""
+Write a function that accepts a list as input and returns a new list which includes every other element in the original list. Keep the first element, i.e. input_list[0]. For example if:
+
+input_list = ["we", "love", "python", "so","much"]
+then your function should return a list such as:
+['we', 'python', 'much']
+"""
+
