3f15fd5f98d7deb4d1e744d3aa5495df28bff878
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-04 16:07:26 +0530

    Part10.py

---
+++ Midterm_Exam/Part10.py
@@ -0,0 +1,9 @@
+"""
+Write a program that asks the user to enter an integer between 1 and 9999 (both inclusive) and then prints the input integer using words. 
+For example if the user enters:
+
+3421
+Then your program should print
+three thousand four hundred twenty one
+"""
+
