11785b23904176623fb372e3b5dda53f23155f5e
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-04 15:21:51 +0530

    Part9.py

---
+++ Midterm_Exam/Part9.py
@@ -0,0 +1,15 @@
+"""
+Write a function named find_gcd that accepts a list of positive integers and returns their greatest common divisor (GCD). Your function 
+should return 1 as the GCD if there are no common factors between the integers. Here are some examples: 
+
+if the list was
+
+[12, 24, 6, 18]
+then your function should return the GCD:
+6
+if the list was
+[3, 5, 9, 11, 13]
+then your function should return their GCD:
+1
+"""
+
