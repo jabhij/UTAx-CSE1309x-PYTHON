dcf8ef6336142bf573c9b0138bebe0e0b02efc61
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-04 18:19:24 +0530

    CE_Ex2.py

---
+++ Week6/Strings/CE_Ex2.py
@@ -0,0 +1,8 @@
+"""
+Write a function that accepts an alphabetic character and returns the number associated with it from the ASCII table.
+"""
+
+#Function Dec.
+def get_asc(n):
+  # Using 
+    return ord(n)
