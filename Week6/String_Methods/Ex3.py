754b69bb44be859c9f688aaa5b9d7340a758677c
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-05 18:06:57 +0530

    Ex3.py

---
+++ Week6/String_Methods/Ex3.py
@@ -0,0 +1,7 @@
+"""
+Write a function which accepts an input string and returns a string where the case of the characters are changed, i.e. all the uppercase 
+characters are changed to lower case and all the lower case characters are changed to upper case. The non-alphabetic characters should not
+be changed. Do NOT use the string methods upper(), lower(), or swap().
+
+"""
+
