67b6ad32dbae764a22d262d045d4ae1b982eb7b9
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-06 18:10:43 +0530

    Ex4.py

---
+++ Week6/String_Methods/Ex4.py
@@ -0,0 +1,5 @@
+"""
+Write a function which accepts an input string consisting of alphabetic characters and spaces and returns the string with all the spaces 
+removed. Do NOT use any string methods for this problem.
+"""
+
