2df61cf0806392135ad513d3b0b4b516c6f7c886
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-06 19:16:49 +0530

    Ex5.py

---
+++ Week6/String_Practice/Ex5.py
@@ -0,0 +1,6 @@
+"""
+Write a function that accepts a string and a character as input and returns the count of all the words in the string which start with the
+given character. Assume that capitalization does not matter here. You can assume that the input string is a sentence i.e. words are
+separated by spaces and consists of alphabetic characters.
+"""
+
