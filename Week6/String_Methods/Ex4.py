cd9b67b1dae4875ee6089a622550f5aed2048e3c
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-06 18:19:26 +0530

    Ex4.py

--- Week6/String_Methods/Ex4.py
+++ Week6/String_Methods/Ex4.py
@@ -3,3 +3,14 @@ Write a function which accepts an input string consisting of alphabetic characte
 removed. Do NOT use any string methods for this problem.
 """
 
+# Function Dec.
+def remove_ws(string):
+    new_string = ' '
+    # Accessing all the characters in string
+    for i in range(1, len(string)):
+        # Selecting only* character
+        # Rejecting whitespaces
+        if string[i] != ' ':
+            # Adding characters to a new string
+            new_string += string[i]
+    return new_string
