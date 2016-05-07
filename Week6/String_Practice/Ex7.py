b9c1d2f84f6e20da4ddc322569c8cab4ef4d4567
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-07 13:15:44 +0530

    Ex7.py

---
+++ Week6/String_Practice/Ex7.py
@@ -0,0 +1,8 @@
+"""
+Write a function that takes a string consisting of alphabetic characters as input argument and returns the most common character. Ignore 
+white spaces i.e. Do not count any white space as a character. Note that capitalization does not matter here i.e. that a lower case 
+character is equal to a upper case character. In case of a tie between certain characters return the last character that has the most 
+count
+
+"""
+
