0ccdc3d5e2f306e11fc717b1c402a2b94a8253f2
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-08 14:16:27 +0530

    Part1.py

--- ASSIGNMENT-2/Part1.py
+++ ASSIGNMENT-2/Part1.py
@@ -10,7 +10,33 @@ First string	   Second String  	Function return
 Python	         Java	               2
 Hello There      helloothere	       1
 sin              sink           	2 (note not the same length)
-dog	          Dog	            0
+dog	          Dog	               0
 
 """
 
+# Function Dec.
+def find_mismatch(str1, str2):
+    # Changing the case
+    str1 = str1.upper()
+    str2 = str2.upper()
+    # If strings aren't of same length
+    if len(str1) != len(str2):
+        return 2
+
+    count = 0
+    # Accessing chars of string 1
+    for i in range(len(str1)):
+        # Comparing strings and counting mismatch
+        if str1[i] != str2[i]:
+            count += 1
+            # Case1
+            if count > 1:
+                return 2
+            # Case2
+            elif (count < 2) and (len(str1) == len(str2)):
+                return 1
+            # Case3
+            elif (count == 0) and (len(str1) == len(str2)):
+                return 0
+                
+                
