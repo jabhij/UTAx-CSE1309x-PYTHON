efa1554ecb068c2072475052ab7a3409791a9574
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-09 16:29:51 +0530

    Part2.py

--- ASSIGNMENT-2/Part2.py
+++ ASSIGNMENT-2/Part2.py
@@ -20,3 +20,39 @@ programing	programming	   1 (Inserting a single character)
 
 """
 
+# Function Dec.
+def single_insert_or_delete(str1, str2):
+    str1 = str1.upper()
+    str2 = str2.upper()
+    # If strings are exactly same
+    if str1 == str2:
+        return 0
+
+    # If neither length nor value is same
+    # or maybe one
+    if str1 == str2:
+        if len(str1) != len(str2):
+            return 2
+        return 2
+
+    # Deletion case
+    # In case of deletion length of str1
+    # must be greater than str2
+    if len(str1) > len(str2):
+        for i in range(len(str1)):
+            # Checking the necessary conditions
+            if str1[i] != str2[i]:
+                if str1[i+1:] == str2[i:]:
+                    return 1
+                else:
+                    return 2
+
+    # Insertion case
+    else:
+        for i in range(len(str1)):
+            # Checking the necessary conditions
+            if str1[i] != str2[i]:
+                if str1[i:] == str2[i+1:]:
+                    return 1
+                else:
+                    return 2
