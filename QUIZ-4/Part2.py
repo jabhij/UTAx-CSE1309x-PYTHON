61ebf318f80ae5ce7b4c18ada34e4054650140b4
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-10 18:07:37 +0530

    Part2.py

---
+++ QUIZ-4/Part2.py
@@ -0,0 +1,8 @@
+"""
+Write a function named count_consonants that receives a string as parameter and returns the total count of the consonants in the string.
+Consonants are all the characters in the english alphabet except for 'a', 'e', 'i', 'o', 'u'. If the same consonant repeats multiple times
+you should count all of them. Note that capitalization and punctuation does not matter here i.e. a lower case character should be considered 
+the same as an upper case character.
+
+"""
+
