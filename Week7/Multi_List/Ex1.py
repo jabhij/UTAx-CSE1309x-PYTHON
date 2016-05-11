6c717dc90267012c28ea0aaf8b097fcd4dc30759
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-11 16:52:28 +0530

    Ex1.py

---
+++ Week7/Multi_List/Ex1.py
@@ -0,0 +1,6 @@
+"""
+Write a function which accepts a 2D list of numbers and returns the sum of all the number in the list You can assume that the number of 
+columns in each row is the same. (Do not use python's built-in sum() function).
+
+"""
+
