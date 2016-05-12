fbed223b6e13d289f2e5ad9af6b3f2f822b565ea
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-12 16:12:33 +0530

    Ex3.py

---
+++ Week7/Multi_List/Ex3.py
@@ -0,0 +1,7 @@
+"""
+Write a function that accepts a 2D list of integers and returns the maximum EVEN value for the entire list. You can assume that the number
+of columns in each row is the same. Your function should return None if the list is empty or all the numbers in the 2D list are odd. 
+Do NOT use python's built in max() function.
+
+"""
+
