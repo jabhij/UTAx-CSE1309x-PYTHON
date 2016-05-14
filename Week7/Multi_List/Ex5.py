3732185e71b086ca93204f00ce8dcc2b840e7da5
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-14 20:08:41 +0530

    Ex5.py

---
+++ Week7/Multi_List/Ex5.py
@@ -0,0 +1,6 @@
+"""
+Write a function that takes a two-dimensional list (list of lists) of numbers as argument and returns a list which includes the sum of 
+each column. Assume that the number of columns in each row is the same.
+
+"""
+
