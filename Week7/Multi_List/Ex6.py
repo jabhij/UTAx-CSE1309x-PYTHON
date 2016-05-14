2a49ae2648689c6e45f3c88f27caa10bdac1ce83
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-14 21:25:08 +0530

    Ex6.py

---
+++ Week7/Multi_List/Ex6.py
@@ -0,0 +1,7 @@
+"""
+Write a function that will receive a 2D list of integers. The function should return the count of how many rows of the list have even sums
+and the count of how many rows have odd sums. 
+For example if the even count was 2, and odd count was 4 your function should return them in a list like this: [2, 4].
+
+"""
+
