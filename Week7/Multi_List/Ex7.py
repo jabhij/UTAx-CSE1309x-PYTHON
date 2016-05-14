439db5c00d062269d583e9d4dfceb1533023db72
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-14 21:36:51 +0530

    Ex7.py

--- Week7/Multi_List/Ex7.py
+++ Week7/Multi_List/Ex7.py
@@ -3,3 +3,12 @@ Write a function that takes a two-dimensional list (list of lists) of numbers as
 value of each row.
 
 """
+
+# Function Dec.
+def get_max(L):
+    new_list = []
+    # Iterating through rows
+    for i in L:
+        # Max and Append
+        new_list.append(max(i))
+    return new_list
