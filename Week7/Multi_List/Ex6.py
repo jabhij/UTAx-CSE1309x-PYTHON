8b5952f57e77bf5ead84ca7270ad869929ecccc1
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-14 21:32:12 +0530

    Ex6.py

--- Week7/Multi_List/Ex6.py
+++ Week7/Multi_List/Ex6.py
@@ -5,3 +5,18 @@ For example if the even count was 2, and odd count was 4 your function should re
 
 """
 
+# Function Dec.
+def get_rows_count(L):
+    new_list = []
+    # Iterating through Lists
+    for i in L:
+        new_list.append(sum(i))
+        even_count = 0
+        odd_count = 0
+        # Checking Even-Odd
+        for j in new_list:
+            if j % 2 == 0:
+                even_count += 1
+            else:
+                odd_count += 1
+    return [even_count, odd_count]
