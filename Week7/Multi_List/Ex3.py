c2fbae8eb818284b82d1f75bc24ccf00051387b4
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-14 19:38:26 +0530

    Ex3.py

--- Week7/Multi_List/Ex3.py
+++ Week7/Multi_List/Ex3.py
@@ -5,3 +5,20 @@ Do NOT use python's built in max() function.
 
 """
 
+# Type your code here
+def even_val(L):
+    if not L:
+        return None
+    new_list = []
+    for row in range(len(L)):
+        for col in range(len(L[0])):
+            if L[row][col] % 2 == 0:
+                new_list.append(L[row][col])
+    return new_list
+
+def max_even_val(new_list):
+    max_even = new_list[0]
+    for i in new_list:
+        if i > max_even:
+            max_even = i
+    return max_even
