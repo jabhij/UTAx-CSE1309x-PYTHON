ff7f9a8b4f5e5061fb0957e166058eeec0cf6290
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-14 22:27:00 +0530

    Ex8.py

---
+++ Week7/Multi_List/Ex8.py
@@ -0,0 +1,6 @@
+"""
+Write a function that takes a two-dimensional list (list of lists) of numbers as argument and returns a list which includes the maximum 
+value of each column. Assume that the length of columns is consistent in each row.
+
+"""
+
