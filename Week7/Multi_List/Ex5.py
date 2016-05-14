c5d49d426275cb61100aaa911961b6fdffc1b060
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-14 21:23:15 +0530

    Ex5.py

--- Week7/Multi_List/Ex5.py
+++ Week7/Multi_List/Ex5.py
@@ -4,3 +4,15 @@ each column. Assume that the number of columns in each row is the same.
 
 """
 
+# Function Dec.
+def cols_sum(L):
+    new_list = []
+    # Starting from first Col
+    for i in range(len(L[0])):
+        get_sum = 0
+        # Iterating through rows (top -> bottom)
+        for j in L:
+            # Add and Append
+            get_sum += j[i]
+        new_list.append(get_sum)
+    return new_list
