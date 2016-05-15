209d0f302d08e40fd6fb6a2d5c8332a5d6e1288f
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-16 02:59:51 +0530

    Ex12.py

---
+++ Week7/Multi_List/Ex12.py
@@ -0,0 +1,17 @@
+"""
+Write a function that accepts two (matrices) 2 dimensional lists a and b of unknown lengths and returns their product.
+Hint: Two matrices a and b can be multiplied together only if the number of columns of the first matrix(a) is the same as the number of 
+rows of the second matrix(b). Hint: You may import and use the numpy module but your return must be a python list not a numpy array. 
+The input for this function will be two 2 Dimensional lists. For example if the input lists are:
+
+a = [[2, 3, 4],
+     [3, 4, 5]]
+b = [[4, -3, 12],
+     [1, 1, 5],
+     [1, 3, 2]]
+     
+Then your function should return their product such as:
+[[15, 9, 47], [21, 10, 66]] 
+
+"""
+
