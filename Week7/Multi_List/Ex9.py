64bdf95f7efd70cfd070d9a6c71e07e2246ede49
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-16 00:16:59 +0530

    Ex9.py

--- Week7/Multi_List/Ex9.py
+++ Week7/Multi_List/Ex9.py
@@ -4,3 +4,13 @@ the integers from the original 2-dimensional list.
 
 """
 
+# Function Dec.
+def _2D_to_1D_ (L):
+    new_list = []
+    # Accessing Rows
+    for i in L:
+        # Accessing every row's element
+        for elements in i:
+            new_list.append(elements)
+            new_list.sort()
+    return new_list
