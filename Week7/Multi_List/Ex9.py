ded46ef4d02a62f4f9c4698d720463d828a117fb
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-15 20:54:37 +0530

    Ex9.py

---
+++ Week7/Multi_List/Ex9.py
@@ -0,0 +1,6 @@
+"""
+Write a function that accepts a 2-dimensional list of integers, and returns a sorted (ascending order) 1-Dimensional list containing all 
+the integers from the original 2-dimensional list.
+
+"""
+
