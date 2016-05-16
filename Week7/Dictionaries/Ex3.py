247836990290a7972af10e28b5ceeb95a620b65d
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-16 19:08:56 +0530

    Ex3.py

---
+++ Week7/Dictionaries/Ex3.py
@@ -0,0 +1,7 @@
+"""
+Write a function that accepts a dictionary as input which contains the names and grades of students (The keys are student names and the 
+values are lists of grades for 3 exams (1 Dimensional list)) and returns the list of names for those students whose grade on all three 
+exams is greater than or equal to 78.
+
+"""
+
