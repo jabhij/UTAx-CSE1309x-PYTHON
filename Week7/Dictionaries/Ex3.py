b64abfe5b794cec63ba48ceee9bac6765a0e93b7
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-17 18:57:16 +0530

    Ex3.py

--- Week7/Dictionaries/Ex3.py
+++ Week7/Dictionaries/Ex3.py
@@ -5,3 +5,13 @@ exams is greater than or equal to 78.
 
 """
 
+# Function Dec.
+def get_grades(dict):
+    new_list = []
+    get_keys = dict.keys()
+    for key in get_keys:
+        lists = dict[key]
+        grade1, grade2, grade3 = lists[0], lists[1], lists[3]
+        if grade1 >= 78 and grade2 >= 78 and grade3 >= 78:
+            new_list.append(key)
+    return new_list
