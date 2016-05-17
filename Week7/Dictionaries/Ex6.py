eb6809bd4abf8a608fb00427c9d05ecad0e05dff
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-17 21:47:27 +0530

    Create Ex6.py

---
+++ Week7/Dictionaries/Ex6.py
@@ -0,0 +1,7 @@
+"""
+Write a function that takes a string as input argument and returns a dictionary of vowel counts i.e. the keys of this dictionary should 
+be individual vowels and the values should be the total count of those vowels. You should ignore white spaces and they should not be 
+counted as a character. Also note that a small letter vowel is equal to a capital letter vowel.
+
+"""
+
