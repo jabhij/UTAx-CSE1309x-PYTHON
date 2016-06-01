2ebfe84a84b49745f617b2056df3ef678f642641
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-01 17:18:27 +0530

    Create Ex9.py

---
+++ Week7/Dictionaries/Ex9.py
@@ -0,0 +1,16 @@
+"""
+Write a function that accepts a string which contains a particular date from the Gregorian calendar. Your function should return what 
+day of the week it was. Here are a few examples of what the input string(Month Day Year) will look like. However, you should not 'hardcode'
+your program to work only for these input!
+
+"June 12 2012"
+"September 3 1955"
+"August 4 1843" 
+
+Note that each item (Month Day Year) is separated by one space. For example if the input string is:
+"May 5 1992"
+
+Then your function should return the day of the week (string) such as:
+"Tuesday"
+"""
+
